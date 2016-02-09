6b15f31caf084480cfb3ba55295cdf13aedb54e9
Darrin Willis <WillisDarrin@gmail.com>
2016-02-08 17:28:46 -0800

    Added fonts, dircolors, and a submodule for the terminal

--- setup.sh
+++ setup.sh
@@ -11,7 +11,11 @@ set -e
 
 DIR=`dirname ${BASH_SOURCE[0]}`
 
-GNOMECONF='gnome-terminal-conf.xml'
+GNOMECONF="$DIR/gnome-terminal-conf.xml"
+FONTS="$DIR/fonts"
+VIMRC="$DIR/vimrc"
+DIRCOLORS="$DIR/dircolors"
+TERM_COLORS="$DIR/gnome-terminal-colors-solarized"
 
 ORIG_DIR=`pwd`
 
@@ -21,12 +25,23 @@ echo "Setting up development environment"
 echo "Installing vim"
 sudo apt-get install vim
 
+echo "Installing fonts"
+mkdir -p ~/.fonts
+cp -rf "$FONTS/*" ~/.fonts
+
+echo "Installing Solarized"
+cd $TERM_COLORS
+./set_dark.sh
+
+echo "Installing dircolors fix"
+cp $DIRCOLORS ~/.dircolors
+eval `dircolors ~/.dircolors`
+
 if [ -e "~/.vim" ]; then
     echo "Backing up ~/.vim to ~/.vim.bak"
     mv ~/.vim ~/.vim.bak
     mkdir -p ~/.vim/bundle
 fi
-
 
 echo "Installing Vundle"
 git clone https://github.com/gmarik/Vundle.vim ~/.vim/bundle/Vundle.vim
@@ -35,14 +50,14 @@ echo "Backing up ~/.vimrc to ~/.vimrc.bak"
 mv ~/.vimrc ~/.vimrc.bak
 
 echo "Installing new .vimrc"
-cp $DIR/.vimrc ~/.vimrc
+cp $VIMRC ~/.vimrc
 
 echo "Installing plugins"
 vim +PluginInstall +qall
 
 # Setup terminal profile
-echo "Applying profile settings"
-gconftool-2 --load $DIR/$GNOMECONF
+# echo "Applying profile settings"
+# gconftool-2 --load $GNOMECONF
 
 cd $ORIG_DIR
 echo "Done"
